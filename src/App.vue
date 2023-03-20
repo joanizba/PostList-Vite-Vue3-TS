b17c5da03ca9754c96df6d330f2336e7d1136dcf
joanizba <joanizquierdo07@gmail.com>
2023-03-20 19:18:53 +0100

    project postlist vue3-TS

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import PostList from './components/PostList.vue';
+</script>
+
+<template>
+  <PostList class="post"/>
+</template>
+
+<style scoped>
+  
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>Listado de posts consumiendo Placeholder-post API</h1>
+  <ul class="post-list">
+    <li 
+        v-for="post in posts" 
+        :key="post.id">
+        <h2>{{ post.title }}</h2>
+        <p> {{ post.body }}</p>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+    import PostService from '../services/PostService';
+    import {onMounted, ref} from 'vue'
+
+    const service = new PostService()
+    const posts = service.getPosts()
+
+    onMounted( async ()=>{
+        await service.fetchAll()
+    })
+
+
+</script>
+
+<style scoped>
+  .post-list{
+    width: auto;
+    height: 75px;
+    padding: 20px;
+    list-style-type: none;
+  }
+  .post-list li {
+    padding: 10px;
+    width: 100%;
+    border: 1px solid #ccc;
+  }
+  h1{
+    color: rgb(96, 129, 238);
+    font-size: 45px;
+    margin: auto 5em;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
